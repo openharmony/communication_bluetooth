9749ef5449c9b47bb848ec7e220a334abf6f873b
lee <lilong32@huawei.com>
2022-03-09 19:25:06 +0800

    fixed 1a31bc4 from https://gitee.com/lilong32/communication_bluetooth/pulls/91 bluetooth rffcom and crash bugfix
    Signed-off-by: lee <lilong32@huawei.com>

--- services/bluetooth_standard/hardware/src/bluetooth_hdi.cpp
+++ services/bluetooth_standard/hardware/src/bluetooth_hdi.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2021 Huawei Device Co., Ltd.
+ * Copyright (C) 2021-2022 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -83,7 +83,6 @@ int HdiSendHciPacket(BtPacketType type, const BtPacket *packet)
 void HdiClose()
 {
     if (g_bluetoothHciCallbacks != nullptr) {
-        delete g_bluetoothHciCallbacks;
         g_bluetoothHciCallbacks = nullptr;
     }
 
