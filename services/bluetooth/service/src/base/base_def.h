9944fcf01f9da59fb798c2659193636a40bfca7d
lee <lilong32@huawei.com>
2022-09-22 11:29:44 +0800

    bluetooth沙箱整改
    Signed-off-by: lee <lilong32@huawei.com>

--- services/bluetooth/service/src/base/base_def.h
+++ services/bluetooth/service/src/base/base_def.h
@@ -60,7 +60,7 @@
  * @brief Bluetooth system config path.
  */
 const std::string BT_CONFIG_PATH_BASE = "/system/etc/bluetooth/";
-const std::string BT_CONFIG_PATH = "/data/bluetooth/";
+const std::string BT_CONFIG_PATH = "/data/service/el1/public/bluetooth/";
 
 
 #ifndef CHECK_EXCEPT_INTR
