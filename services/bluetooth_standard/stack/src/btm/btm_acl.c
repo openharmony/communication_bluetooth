83a54404a03aacf8bc7dc3a31f9497af8eee78fb
fengjiahui4 <fengjiahui4@huawei.com>
2022-10-26 15:06:38 +0800

    sync from master : vaild alloc nullptr
    Signed-off-by: fengjiahui4 <fengjiahui4@huawei.com>

--- services/bluetooth_standard/stack/src/btm/btm_acl.c
+++ services/bluetooth_standard/stack/src/btm/btm_acl.c
@@ -664,7 +664,11 @@ static int BtmLeExtendedCreateConnection()
     uint16_t leScanWindow = g_leScanWindow;
 
     HciLeConnectionParamSet *sets = MEM_MALLOC.alloc(sizeof(HciLeConnectionParamSet) * countOfSets);
-
+    if (sets == NULL) {
+        LOG_ERROR("sets is NULL");
+        return BT_NO_MEMORY;
+    }
+    
     for (uint8_t i = 0; i < countOfSets; i++) {
         sets[i].scanInterval = leScanInterval;
         sets[i].scanWindow = leScanWindow;
