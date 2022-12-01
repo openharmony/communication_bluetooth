3797b05ece301adaec55a81d3293f91cb7e3e518
liuqian_herb <704938153@qq.com>
2022-12-01 10:37:19 +0800

    cherry pick 1018
    Signed-off-by: liuqian_herb <704938153@qq.com>

--- frameworks/js/napi/src/napi_bluetooth_gatt_client_callback.cpp
+++ frameworks/js/napi/src/napi_bluetooth_gatt_client_callback.cpp
@@ -13,7 +13,9 @@
  * limitations under the License.
  */
 #include <uv.h>
+#include <unistd.h>
 #include "bluetooth_log.h"
+#include "napi_bluetooth_event.h"
 #include "napi_bluetooth_gatt_client.h"
 #include "napi_bluetooth_gatt_client_callback.h"
 
@@ -22,6 +24,7 @@ namespace Bluetooth {
 void NapiGattClientCallback::OnCharacteristicChanged(const GattCharacteristic &characteristic)
 {
     HILOGI("enter");
+    std::unique_lock<std::mutex> guard(g_gattClientCallbackInfosMutex);
     if (!callbackInfos_[STR_BT_GATT_CLIENT_CALLBACK_BLE_CHARACTERISTIC_CHANGE]) {
         HILOGI("This callback is not registered by ability.");
         return;
@@ -29,12 +32,21 @@ void NapiGattClientCallback::OnCharacteristicChanged(const GattCharacteristic &c
     std::shared_ptr<GattCharacteristicCallbackInfo> callbackInfo =
         std::static_pointer_cast<GattCharacteristicCallbackInfo>(
             callbackInfos_[STR_BT_GATT_CLIENT_CALLBACK_BLE_CHARACTERISTIC_CHANGE]);
+
     HILOGI("uuid is %{public}s", characteristic.GetUuid().ToString().c_str());
     callbackInfo->characteristic_ = characteristic;
     uv_loop_s *loop = nullptr;
     napi_get_uv_event_loop(callbackInfo->env_, &loop);
     uv_work_t *work = new uv_work_t;
+    if (work == nullptr) {
+        HILOGI("uv_work_t work is null.");
+        return;
+    }
     work->data = (void*)callbackInfo.get();
+    uint32_t refCount = INVALID_REF_COUNT;
+    napi_reference_ref(callbackInfo->env_, callbackInfo->callback_, &refCount);
+    HILOGI("increments the reference count, refCount: %{public}d", refCount);
+
     uv_queue_work(
         loop,
         work,
@@ -49,7 +61,16 @@ void NapiGattClientCallback::OnCharacteristicChanged(const GattCharacteristic &c
             napi_value callResult = nullptr;
             napi_get_undefined(callbackInfo->env_, &undefined);
             napi_get_reference_value(callbackInfo->env_, callbackInfo->callback_, &callback);
-            napi_call_function(callbackInfo->env_, undefined, callback, ARGS_SIZE_ONE, &result, &callResult);
+            if (callback != nullptr) {
+                HILOGI("napi_call_function called");
+                napi_call_function(callbackInfo->env_, undefined, callback, ARGS_SIZE_ONE, &result, &callResult);
+            }
+            uint32_t refCount = INVALID_REF_COUNT;
+            napi_reference_unref(callbackInfo->env_, callbackInfo->callback_, &refCount);
+            HILOGI("uv_queue_work unref, refCount: %{public}d", refCount);
+            if (refCount == 0) {
+                napi_delete_reference(callbackInfo->env_, callbackInfo->callback_);
+            }
             delete work;
             work = nullptr;
         }
@@ -91,20 +112,28 @@ void NapiGattClientCallback::OnDescriptorReadResult(const GattDescriptor &descri
 void NapiGattClientCallback::OnConnectionStateChanged(int connectionState, int ret)
 {
     HILOGI("enter");
-    if (!callbackInfos_[STR_BT_GATT_CLIENT_CALLBACK_BLE_CONNECTIION_STATE_CHANGE]) {
+    std::unique_lock<std::mutex> guard(g_gattClientCallbackInfosMutex);
+    std::map<std::string, std::shared_ptr<BluetoothCallbackInfo>>::iterator it =
+    callbackInfos_.find(STR_BT_GATT_CLIENT_CALLBACK_BLE_CONNECTIION_STATE_CHANGE);
+    if (it == callbackInfos_.end() || it->second == nullptr) {
         HILOGI("This callback is not registered by ability.");
         return;
     }
-    std::shared_ptr<BluetoothCallbackInfo> callbackInfo =
-        callbackInfos_[STR_BT_GATT_CLIENT_CALLBACK_BLE_CONNECTIION_STATE_CHANGE];
-
+    std::shared_ptr<BluetoothCallbackInfo> callbackInfo = it->second;
+    HILOGI("connectionState:%{public}d, ret:%{public}d", connectionState, ret);
     callbackInfo->state_ = connectionState;
     callbackInfo->deviceId_ = client_->GetDevice()->GetDeviceAddr();
     uv_loop_s *loop = nullptr;
     napi_get_uv_event_loop(callbackInfo->env_, &loop);
     uv_work_t *work = new uv_work_t;
+    if (work == nullptr) {
+        HILOGI("uv_work_t work is null.");
+        return;
+    }
     work->data = (void*)callbackInfo.get();
-
+    uint32_t refCount = INVALID_REF_COUNT;
+    napi_reference_ref(callbackInfo->env_, callbackInfo->callback_, &refCount);
+    HILOGI("increments the reference count, refCount: %{public}d", refCount);
     uv_queue_work(
         loop,
         work,
@@ -113,13 +142,22 @@ void NapiGattClientCallback::OnConnectionStateChanged(int connectionState, int r
             BluetoothCallbackInfo *callbackInfo = (BluetoothCallbackInfo *)work->data;
             napi_value result = nullptr;
             napi_create_object(callbackInfo->env_, &result);
-            ConvertStateChangeParamToJS(callbackInfo->env_, result, callbackInfo->deviceId_, callbackInfo->state_);      
+            ConvertStateChangeParamToJS(callbackInfo->env_, result, callbackInfo->deviceId_, callbackInfo->state_);
             napi_value callback = nullptr;
             napi_value undefined = nullptr;
             napi_value callResult = nullptr;
             napi_get_undefined(callbackInfo->env_, &undefined);
             napi_get_reference_value(callbackInfo->env_, callbackInfo->callback_, &callback);
-            napi_call_function(callbackInfo->env_, undefined, callback, ARGS_SIZE_ONE, &result, &callResult);
+            if (callback != nullptr) {
+                HILOGI("napi_call_function called");
+                napi_call_function(callbackInfo->env_, undefined, callback, ARGS_SIZE_ONE, &result, &callResult);
+            }
+            uint32_t refCount = INVALID_REF_COUNT;
+            napi_reference_unref(callbackInfo->env_, callbackInfo->callback_, &refCount);
+            HILOGI("uv_queue_work unref, refCount: %{public}d", refCount);
+            if (refCount == 0) {
+                napi_delete_reference(callbackInfo->env_, callbackInfo->callback_);
+            }
             delete work;
             work = nullptr;
         }
