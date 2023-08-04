78463d920adf148cd2bf75a3fab1a43127904c52
18082231538 <1399410084@qq.com>
2023-08-04 15:52:11 +0800

    bluetooth alarm modify
    Signed-off-by: 18082231538 <1399410084@qq.com>

--- frameworks/inner/src/bluetooth_avrcp_ct.cpp
+++ frameworks/inner/src/bluetooth_avrcp_ct.cpp
@@ -1003,21 +1003,17 @@ void AvrcpController::UnregisterObserver(IObserver *observer)
 std::vector<BluetoothRemoteDevice> AvrcpController::GetConnectedDevices(void)
 {
     HILOGI("enter");
-    if (pimpl == nullptr) {
-        HILOGE("pimpl is nullptr");
-        return std::vector<BluetoothRemoteDevice>();
-    }
-    std::lock_guard<std::mutex> lock(pimpl->observerMutex_);
     if (!IS_BT_ENABLED()) {
         HILOGE("bluetooth is off.");
         return std::vector<BluetoothRemoteDevice>();
     }
 
-    if (!pimpl->proxy_) {
-        HILOGE("avrcpCt proxy_ is nullptr");
+    if (pimpl == nullptr || !pimpl->proxy_) {
+        HILOGE("pimpl or avrcpCt proxy_ is nullptr");
         return std::vector<BluetoothRemoteDevice>();
     }
 
+    std::lock_guard<std::mutex> lock(pimpl->observerMutex_);
     std::vector<BluetoothRemoteDevice> devices;
     std::vector<RawAddress> rawAddrs = pimpl->proxy_->GetConnectedDevices();
     for (auto rawAddr : rawAddrs) {
