16547935f86f9437b4c8d8d420f2a6575b95e775
mayujie <mayujie8@huawei.com>
2023-10-28 14:55:04 +0800

    add absolute volume interface
    Signed-off-by: mayujie <mayujie8@huawei.com>

--- frameworks/js/napi/include/napi_bluetooth_a2dp_src.h
+++ frameworks/js/napi/include/napi_bluetooth_a2dp_src.h
@@ -41,7 +41,11 @@ public:
     static napi_value SetConnectionStrategy(napi_env env, napi_callback_info info);
     static napi_value GetConnectionStrategy(napi_env env, napi_callback_info info);
     static napi_value GetConnectionState(napi_env env, napi_callback_info info);
-    static napi_value getConnectedDevices(napi_env env, napi_callback_info info);
+    static napi_value GetConnectedDevices(napi_env env, napi_callback_info info);
+    static napi_value IsAbsoluteVolumeSupport(napi_env env, napi_callback_info info);
+    static napi_value IsAbsoluteVolumeEnabled(napi_env env, napi_callback_info info);
+    static napi_value EnableAbsoluteVolume(napi_env env, napi_callback_info info);
+    static napi_value DisableAbsoluteVolume(napi_env env, napi_callback_info info);
 #endif
     static NapiA2dpSourceObserver observer_;
     static bool isRegistered_;
