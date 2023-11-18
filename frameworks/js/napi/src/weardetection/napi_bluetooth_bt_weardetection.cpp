f0cde150d6a632c4c67a30c98c3ebfd73131d1dc
wangzongze <13723159+wangzongze28@user.noreply.gitee.com>
2023-11-18 11:14:42 +0000

    update time
    Signed-off-by: wangzongze <wangzongze@huawei.com>

--- frameworks/js/napi/src/weardetection/napi_bluetooth_bt_weardetection.cpp
+++ frameworks/js/napi/src/weardetection/napi_bluetooth_bt_weardetection.cpp
@@ -4,7 +4,10 @@
 =======
 /*
  * Copyright (c) Huawei Technologies Co., Ltd. 2023-2023. All rights reserved.
+<<<<<<< HEAD
 >>>>>>> 399c6857 (update time)
+=======
+>>>>>>> cf103f81 (update time)
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -18,10 +21,14 @@
  * limitations under the License.
  */
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 =======
 
 >>>>>>> 399c6857 (update time)
+=======
+
+>>>>>>> cf103f81 (update time)
 #include <uv.h>
 #include "napi_bluetooth_bt_weardetection.h"
 #include "bluetooth_audio_manager.h"
