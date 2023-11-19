8548312211022b2397277bd65d1b82a23383826d
wangzongze <wangzongze@huawei.com>
2023-11-19 11:21:22 +0000

    upate copyright
    Signed-off-by: wangzongze <wangzongze@huawei.com>

--- frameworks/inner/ipc/src/bluetooth_audio_manager_proxy.cpp
+++ frameworks/inner/ipc/src/bluetooth_audio_manager_proxy.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) Huawei Technologies Co., Ltd. 2023-2023. All rights reserved.
+ * Copyright (C) 2023 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -12,6 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 #include "bluetooth_errorcode.h"
 #include "bluetooth_audio_manager_proxy.h"
 #include "bluetooth_log.h"
