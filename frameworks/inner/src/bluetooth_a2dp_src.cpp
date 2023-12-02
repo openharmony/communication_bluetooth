bfdeb5286867f3e97e6af28e41d7c3e03db702d7
mayujie <mayujie8@huawei.com>
2023-12-02 17:50:54 +0800

    modify check device addr and stream size when session request
    Signed-off-by: mayujie <mayujie8@huawei.com>

--- frameworks/inner/src/bluetooth_a2dp_src.cpp
+++ frameworks/inner/src/bluetooth_a2dp_src.cpp
@@ -723,10 +723,12 @@ int A2dpSource::OffloadStopPlaying(const BluetoothRemoteDevice &device, const st
 int A2dpSource::A2dpOffloadSessionRequest(const BluetoothRemoteDevice &device, const std::vector<A2dpStreamInfo> &info)
 {
     HILOGI("enter, session request device:%{public}s", GET_ENCRYPT_ADDR(device));
-    CHECK_AND_RETURN_LOG_RET(IS_BT_ENABLED(), BT_ERR_INVALID_STATE, "bluetooth is off.");
-    CHECK_AND_RETURN_LOG_RET((pimpl != nullptr && pimpl->proxy_ != nullptr), BT_ERR_UNAVAILABLE_PROXY,
+    CHECK_AND_RETURN_LOG_RET(device.IsValidBluetoothRemoteDevice(), A2DP_STREAM_ENCODE_UNKNOWN, "device err");
+    CHECK_AND_RETURN_LOG_RET(device.GetDeviceAddr() != INVALID_MAC_ADDRESS, A2DP_STREAM_ENCODE_UNKNOWN, "invaild mac");
+    CHECK_AND_RETURN_LOG_RET(sessionsId.size() != 0, A2DP_STREAM_ENCODE_SOFTWARE, "empty stream");
+    CHECK_AND_RETURN_LOG_RET(IS_BT_ENABLED(), A2DP_STREAM_ENCODE_UNKNOWN, "bluetooth is off.");
+    CHECK_AND_RETURN_LOG_RET((pimpl != nullptr && pimpl->proxy_ != nullptr), A2DP_STREAM_ENCODE_UNKNOWN,
         "pimpl or a2dpSrc proxy is nullptr");
-    CHECK_AND_RETURN_LOG_RET(device.IsValidBluetoothRemoteDevice(), BT_ERR_INVALID_PARAM, "device err");
 
     std::vector<BluetoothA2dpStreamInfo> streamsInfo = {};
     BluetoothA2dpStreamInfo streamInfo;
