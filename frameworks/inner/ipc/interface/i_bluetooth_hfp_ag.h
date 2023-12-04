5f1e2bd30bdf340dc9a4bf0f02a2de4324a448fe
18082231538 <1399410084@qq.com>
2023-12-04 20:03:53 +0800

    sopport voip call
    Signed-off-by: 18082231538 <1399410084@qq.com>

--- frameworks/inner/ipc/interface/i_bluetooth_hfp_ag.h
+++ frameworks/inner/ipc/interface/i_bluetooth_hfp_ag.h
@@ -18,6 +18,7 @@
 #include <string>
 #include <vector>
 #include "bluetooth_service_ipc_interface_code.h"
+#include "bluetooth_phone_state.h"
 #include "i_bluetooth_hfp_ag_observer.h"
 
 namespace OHOS {
@@ -35,7 +36,7 @@ public:
     virtual int GetScoState(const BluetoothRawAddress &device) = 0;
     virtual int32_t ConnectSco(uint8_t callType) = 0;
     virtual int32_t DisconnectSco(uint8_t callType) = 0;
-    virtual bool ConnectSco() = 0;
+    virtual bool ConnectSco() = 0; 
     virtual bool DisconnectSco() = 0;
     virtual void PhoneStateChanged(BluetoothPhoneState &phoneState) = 0;
     virtual void ClccResponse(int index, int direction, int status, int mode, bool mpty, const std::string &number,
