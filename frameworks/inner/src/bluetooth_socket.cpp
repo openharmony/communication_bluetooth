d08ace12b08c1e5c64a183772dcb24e04a95ce09
liuqian_herb <704938153@qq.com>
2024-06-25 10:27:57 +0800

    change to sptr
    Signed-off-by: liuqian_herb <704938153@qq.com>

--- frameworks/inner/src/bluetooth_socket.cpp
+++ frameworks/inner/src/bluetooth_socket.cpp
@@ -154,22 +154,22 @@ struct ClientSocket::impl {
         return flags;
     }
 
-    InputStream &GetInputStream()
+    std::shared_ptr<InputStream> GetInputStream()
     {
         HILOGD("enter");
         if (inputStream_ == nullptr) {
             HILOGE("inputStream is NULL, failed. please Connect");
         }
-        return *inputStream_;
+        return inputStream_;
     }
 
-    OutputStream &GetOutputStream()
+    std::shared_ptr<OutputStream> GetOutputStream()
     {
         HILOGD("enter");
         if (outputStream_ == nullptr) {
             HILOGE("outputStream is NULL, failed. please Connect");
         }
-        return *outputStream_;
+        return outputStream_;
     }
 
     BluetoothRemoteDevice &GetRemoteDevice()
@@ -251,10 +251,10 @@ struct ClientSocket::impl {
 
     // socket observer
     sptr<BluetoothSocketObserverImp> observerImp_ = nullptr;
-    std::unique_ptr<InputStream> inputStream_ {
+    std::shared_ptr<InputStream> inputStream_ {
         nullptr
     };
-    std::unique_ptr<OutputStream> outputStream_ {
+    std::shared_ptr<OutputStream> outputStream_ {
         nullptr
     };
     bool Init(std::weak_ptr<ClientSocket> client);
@@ -454,13 +454,13 @@ void ClientSocket::Close()
     return pimpl->Close();
 }
 
-InputStream &ClientSocket::GetInputStream()
+std::shared_ptr<InputStream> ClientSocket::GetInputStream()
 {
     HILOGD("enter");
     return pimpl->GetInputStream();
 }
 
-OutputStream &ClientSocket::GetOutputStream()
+std::shared_ptr<OutputStream> ClientSocket::GetOutputStream()
 {
     HILOGD("enter");
     return pimpl->GetOutputStream();
