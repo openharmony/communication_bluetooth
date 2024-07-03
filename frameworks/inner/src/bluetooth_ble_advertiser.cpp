1abb3f43576b24af5b4c47e5fc3fd56d0d1be312
tanhaoran <tanhaoran@huawei.com>
2024-07-03 01:52:50 +0000

    update frameworks/inner/src/bluetooth_ble_advertiser.cpp.
    Signed-off-by: tanhaoran <tanhaoran@huawei.com>

--- frameworks/inner/src/bluetooth_ble_advertiser.cpp
+++ frameworks/inner/src/bluetooth_ble_advertiser.cpp
@@ -490,7 +490,7 @@ int BleAdvertiser::StopAdvertising(std::shared_ptr<BleAdvertiseCallback> callbac
 
     int ret = proxy->StopAdvertising(advHandle);
     CHECK_AND_RETURN_LOG_RET(pimpl->callbackImp_ != nullptr, BT_ERR_INTERNAL_ERROR, "callbackImp is nullptr");
-    pimpl->callbackImp_->OnStopResultEvent();
+    pimpl->callbackImp_->OnStopResultEvent(ADVERTISE_SUCCESS, advHandle);
     return ret;
 }
 
