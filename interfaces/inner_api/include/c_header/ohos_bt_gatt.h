e79b438ce4a09b57b25d5b20be778f4229ea50f0
dengguanghong <2228266166@qq.com>
2024-10-11 10:29:22 +0800

    Signed-off-by:dengguanghong <2228266166@qq.com>

--- interfaces/inner_api/include/c_header/ohos_bt_gatt.h
+++ interfaces/inner_api/include/c_header/ohos_bt_gatt.h
@@ -939,6 +939,12 @@ int RemoveLpDeviceParam(BtUuid uuid);
  */
 void ClearGlobalResource(void);
 
+/**
+ * @brief Clear global adv resource before thread end.
+ * @since 11
+ */
+void ClearGlobalAdv(void);
+
 #ifdef __cplusplus
 }
 #endif
