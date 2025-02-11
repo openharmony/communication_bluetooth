715ab7f5835613e48eb715dfb74bd412aa7965f6
冯鹏义 <fengpengyi1@huawei.com>
2025-02-11 23:20:08 +0800

    编译告警处理
    Signed-off-by: 冯鹏义 <fengpengyi1@huawei.com>

--- frameworks/inner/src/bluetooth_gatt_client.cpp
+++ frameworks/inner/src/bluetooth_gatt_client.cpp
@@ -770,16 +770,15 @@ std::vector<GattService> &GattClient::GetService()
     std::vector<GattService> gattServices;
     if (!IS_BLE_ENABLED()) {
         HILOGE("bluetooth is off.");
-        return gattServices;
+        return pimpl->gattServices_;
     }
 
     if (pimpl == nullptr || !pimpl->Init(weak_from_this())) {
         HILOGE("pimpl or gatt client proxy is nullptr");
-        return gattServices;
+        return pimpl->gattServices_;
     }
 
     pimpl->GetServices();
-    std::lock_guard<std::mutex> lock(pimpl->gattServicesMutex_);
     return pimpl->gattServices_;
 }
 
