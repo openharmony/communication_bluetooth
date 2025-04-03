5b39e9af11df2f80f8c2415518d992adf7b00a7a
fengyong <fengyong21@h-partners.com>
2025-04-03 10:08:27 +0000

    update frameworks/inner/src/bluetooth_socket_outputstream.cpp.
    Signed-off-by: fengyong <fengyong21@h-partners.com>

--- frameworks/inner/src/bluetooth_socket_outputstream.cpp
+++ frameworks/inner/src/bluetooth_socket_outputstream.cpp
@@ -29,7 +29,8 @@ namespace Bluetooth {
 static constexpr int32_t SOCKET_PACKET_HEAD_LENGTH = 1512;
 static constexpr int32_t AAM_UID = 7878;
 static constexpr int32_t AAM_BAD_RET = -978974;
-
+static constexpr int32_t SOFTBUS_UID = 1024;
+static constexpr int32_t SOCKET_WRITE_TIMEOUT_50_SEC = 50;
 OutputStream::OutputStream(int socketFd) : socketFd_(socketFd)
 {}
 
@@ -61,12 +62,25 @@ int OutputStream::Write(const uint8_t *buf, size_t length)
             }
         }
     }
+    if (getuid() == SOFTBUS_UID && setTimeoutFlag_ == false) {
+        HILOGD("SOFTBUS write data set 50s tiemout.");
+        timeval timeout;
+        timeout.tv_sec = SOCKET_WRITE_TIMEOUT_50_SEC;
+        timeout.tv_usec = 0;
+        if (setsockopt(socketFd_, SOL_SOCKET, SO_SNDTIMEO, &timeout, sizeof(timeout)) < 0) {
+            HILOGW("Failed to set send timeout");
+            return -1;
+        }
+        setTimeoutFlag_ = true;
+    }
+
     auto ret = send(socketFd_, buf, length, MSG_NOSIGNAL);
 
     
     HILOGD("ret: %{public}zd", ret);
 
     if (ret <= 0) {
+        setTimeoutFlag_ == false;
         HILOGE("socket write exception! ret:%{public}zd errno:%{public}d", ret, errno);
     }
     return ret;
