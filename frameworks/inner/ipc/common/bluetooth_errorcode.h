f0763dddb1ca2d4122c52827707e53a70299ed25
hui <lizhihui10@huawei.com>
2025-06-26 12:13:36 +0000

    update frameworks/inner/ipc/common/bluetooth_errorcode.h.
    Signed-off-by: hui <lizhihui10@huawei.com>

--- frameworks/inner/ipc/common/bluetooth_errorcode.h
+++ frameworks/inner/ipc/common/bluetooth_errorcode.h
@@ -16,6 +16,6 @@
 #ifndef BLUETOOTH_ERRORCODE_INNER_H
 #define BLUETOOTH_ERRORCODE_INNER_H
 
-#include "bluetooth_errorcode.h"
+#include "foundation/communication/bluetooth/interfaces/inner_api/include/bluetooth_errorcode.h"
 
 #endif  // BLUETOOTH_ERRORCODE_INNER_H
