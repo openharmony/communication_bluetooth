fd41036fc475dd7da614db2e261a08d53f2251f9
guoxiadi <guoxiadi@huawei.com>
2025-09-17 16:08:10 +0800

    bt 0728->master
    Signed-off-by: guoxiadi <guoxiadi@huawei.com>

---
+++ frameworks/ets/taihe/bluetooth_hfp/src/taihe_bluetooth_hfp_ag_observer.cpp
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef LOG_TAG
+#define LOG_TAG "bt_taihe_hfp_ag_observer"
+#endif
+
+#include "stdexcept"
+
+#include "ohos.bluetooth.hfp.proj.hpp"
+#include "ohos.bluetooth.hfp.impl.hpp"
+#include "taihe/runtime.hpp"
+
+#include "bluetooth_log.h"
+#include "bluetooth_utils.h"
+#include "taihe_bluetooth_hfp_ag_observer.h"
+
+namespace OHOS {
+namespace Bluetooth {
+
+TaiheHandsFreeAudioGatewayObserver::TaiheHandsFreeAudioGatewayObserver()
+{}
+
+void TaiheHandsFreeAudioGatewayObserver::OnConnectionStateChanged(const BluetoothRemoteDevice &device,
+    int state, int cause)
+{
+    std::shared_lock<std::shared_mutex> guard(eventSubscribe_.lock_);
+    HILOGD("enter, remote device address: %{public}s, state: %{public}d, cause: %{public}d",
+        GET_ENCRYPT_ADDR(device), state, cause);
+    ::taihe::string deviceId_(device.GetDeviceAddr());
+    int32_t profileConnectionState = TaiheUtils::GetProfileConnectionState(state);
+    auto state_ = ::ohos::bluetooth::constant::ProfileConnectionState::from_value(profileConnectionState);
+    auto cause_ = ::ohos::bluetooth::baseProfile::DisconnectCause::from_value(cause);
+    auto taiheResult = ::ohos::bluetooth::baseProfile::StateChangeParam {
+        .deviceId = deviceId_,
+        .state = state_,
+        .cause = cause_,
+    };
+    for (auto callback : eventSubscribe_.callbackVec_) {
+        if (callback) {
+            (*callback)(taiheResult);
+        }
+    }
+}
+
+void TaiheHandsFreeAudioGatewayObserver::OnScoStateChanged(const BluetoothRemoteDevice &device, int state, int reason)
+{
+    HILOGD("address: %{public}s, state: %{public}d", GET_ENCRYPT_ADDR(device), state);
+}
+
+}  // namespace Bluetooth
+}  // namespace OHOS
---
+++ frameworks/ets/taihe/bluetooth_hid/include/taihe_bluetooth_hid_host_observer.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef TAIHE_BLUETOOTH_HID_HOST_OBSERVER_H_
+#define TAIHE_BLUETOOTH_HID_HOST_OBSERVER_H_
+
+#include <shared_mutex>
+#include "stdexcept"
+
+#include "ohos.bluetooth.hid.proj.hpp"
+#include "ohos.bluetooth.hid.impl.hpp"
+#include "taihe/runtime.hpp"
+
+#include "bluetooth_hid_host.h"
+#include "taihe_event_module.h"
+
+namespace OHOS {
+namespace Bluetooth {
+
+class TaiheBluetoothHidHostObserver : public HidHostObserver {
+public:
+    void OnConnectionStateChanged(const BluetoothRemoteDevice &device, int state, int cause) override;
+    
+    TaiheBluetoothHidHostObserver();
+    ~TaiheBluetoothHidHostObserver() override = default;
+
+private:
+    friend class HidHostProfileImpl;
+    EventModule<void(::ohos::bluetooth::baseProfile::StateChangeParam const& data)> eventSubscribe_;
+};
+}  // namespace Bluetooth
+}  // namespace OHOS
+#endif /* TAIHE_BLUETOOTH_HID_HOST_OBSERVER_H_ */
