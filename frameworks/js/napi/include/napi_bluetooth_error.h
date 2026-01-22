95a8feb63bf0235898accebf493cd7fd388bbb2c
linzlinz <2495670683@qq.com>
2026-01-22 16:06:22 +0800

    mtu bugfix
    Signed-off-by: linzlinz <2495670683@qq.com>

--- frameworks/js/napi/include/napi_bluetooth_error.h
+++ frameworks/js/napi/include/napi_bluetooth_error.h
@@ -24,41 +24,79 @@
 namespace OHOS {
 namespace Bluetooth {
 
+// ENABLE_NAPI_BLUETOOTH_MANAGER is defined only if the API version >= 10.
 #ifdef ENABLE_NAPI_BLUETOOTH_MANAGER
+// NAPI_BT_ASSERT_RETURN indicates that the errMsg is obtained through map, no need to specify errMsg.
 #ifndef NAPI_BT_ASSERT_RETURN
-#define NAPI_BT_ASSERT_RETURN(env, cond, errCode, retObj) \
-do {                                                      \
-    if (!(cond)) {                                        \
-        HandleSyncErr((env), (errCode));                  \
+#define NAPI_BT_ASSERT_RETURN(env, cond, errCode, retObj)       \
+do {                                                            \
+    if (!(cond)) {                                              \
+        HandleSyncErr((env), (errCode));                        \
+        HILOGE("bluetoothManager napi assert failed.");         \
+        return (retObj);                                        \
+    }                                                           \
+} while (0)
+#endif
+// NAPI_BT_ASSERT_RETURN_WITH_MSG indicates that the errMsg is specified by the user.
+#ifndef NAPI_BT_ASSERT_RETURN_WITH_MSG
+#define NAPI_BT_ASSERT_RETURN_WITH_MSG(env, cond, errCode, retObj, errMsg) \
+do {                                                                       \
+    if (!(cond)) {                                                         \
+        HandleSyncErrWithMsg((env), (errCode), (errMsg));                  \
         HILOGE("bluetoothManager napi assert failed.");                    \
-        return (retObj);                                  \
-    }                                                     \
+        return (retObj);                                                   \
+    }                                                                      \
 } while (0)
 #endif
-#else // ENABLE_NAPI_BLUETOOTH_MANAGER
+#else // Under the else branch, the API version < 10, the HandleSyncErr func is not called.
 #ifndef NAPI_BT_ASSERT_RETURN
 #define NAPI_BT_ASSERT_RETURN(env, cond, errCode, retObj) \
 do {                                                      \
     if (!(cond)) {                                        \
-        HILOGE("bluetooth napi assert failed.");                    \
+        HILOGE("bluetooth napi assert failed.");          \
         return (retObj);                                  \
     }                                                     \
 } while (0)
 #endif
+#ifndef NAPI_BT_ASSERT_RETURN_WITH_MSG
+#define NAPI_BT_ASSERT_RETURN_WITH_MSG(env, cond, errCode, retObj, errMsg) \
+do {                                                                       \
+    if (!(cond)) {                                                         \
+        HILOGE("bluetoothManager napi assert failed.");                    \
+        return (retObj);                                                   \
+    }                                                                      \
+} while (0)
+#endif
 #endif
 
-#define NAPI_BT_ASSERT_RETURN_UNDEF(env, cond, errCode)  \
-do {                                                     \
-    napi_value res = nullptr;                            \
-    napi_get_undefined((env), &res);                     \
+#define NAPI_BT_ASSERT_RETURN_UNDEF(env, cond, errCode)   \
+do {                                                      \
+    napi_value res = nullptr;                             \
+    napi_get_undefined((env), &res);                      \
     NAPI_BT_ASSERT_RETURN((env), (cond), (errCode), res); \
 } while (0)
 
-#define NAPI_BT_ASSERT_RETURN_FALSE(env, cond, errCode)  \
-do {                                                     \
-    napi_value res = nullptr;                            \
-    napi_get_boolean((env), false, &res);                \
+// the errMsg is specified by the user.
+#define NAPI_BT_ASSERT_RETURN_UNDEF_WITH_MSG(env, cond, errCode, errMsg)   \
+do {                                                                       \
+    napi_value res = nullptr;                                              \
+    napi_get_undefined((env), &res);                                       \
+    NAPI_BT_ASSERT_RETURN_WITH_MSG((env), (cond), (errCode), res, errMsg); \
+} while (0)
+
+#define NAPI_BT_ASSERT_RETURN_FALSE(env, cond, errCode)   \
+do {                                                      \
+    napi_value res = nullptr;                             \
+    napi_get_boolean((env), false, &res);                 \
     NAPI_BT_ASSERT_RETURN((env), (cond), (errCode), res); \
+} while (0)
+
+// the errMsg is specified by the user.
+#define NAPI_BT_ASSERT_RETURN_FALSE_WITH_MSG(env, cond, errCode, errMsg)   \
+do {                                                                       \
+    napi_value res = nullptr;                                              \
+    napi_get_boolean((env), false, &res);                                  \
+    NAPI_BT_ASSERT_RETURN_WITH_MSG((env), (cond), (errCode), res, errMsg); \
 } while (0)
 
 #define NAPI_BT_ASSERT_NUM_RETURN(env, cond, errCode, retObj) \
@@ -79,6 +117,7 @@ do {                                                          \
 
 std::string GetNapiErrMsg(const napi_env &env, const int32_t errCode);
 void HandleSyncErr(const napi_env &env, int32_t errCode);
+void HandleSyncErrWithMsg(const napi_env &env, int32_t errCode, std::string errMsg);
 void HandleSyncErrNum(const napi_env &env, int32_t errCode);
 }
 }
