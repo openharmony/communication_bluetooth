44e9741a31fb7d6e933ce0b87dc016a4a9b28a6a
songxuelian <1546432050@qq.com>
2026-05-09 18:42:12 +0800

    update: 更新文件 i_bluetooth_ble_advertiser.h
    Signed-off-by: songxuelian <1546432050@qq.com>

--- frameworks/inner/ipc/interface/i_bluetooth_ble_advertiser.h
+++ frameworks/inner/ipc/interface/i_bluetooth_ble_advertiser.h
@@ -44,6 +44,8 @@ public:
         const BluetoothBleAdvertiserData &scanResponse, int32_t advHandle,
         bluetooth::SetAdvDataType type = bluetooth::SET_ADV_DATA_BOTH) = 0;
     virtual int ChangeAdvertisingParams(uint8_t advHandle, const BluetoothBleAdvertiserSettings &settings) = 0;
+    virtual int8_t BleStartRangeAdv(bluetooth::BleAppType appType) = 0;
+    virtual int BleStopRangeAdv(bluetooth::BleAppType appType) = 0;
 };
 }  // namespace Bluetooth
 }  // namespace OHOS
