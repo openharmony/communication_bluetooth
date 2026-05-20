a864440e16cc41ed4b5c5a4895807338ffb88c57
songxuelian <1546432050@qq.com>
2026-05-20 21:45:40 +0800

    upload: 上传文件
    Signed-off-by: songxuelian <1546432050@qq.com>

---
+++ frameworks/inner/ipc/interface/i_bluetooth_device_battery_observer.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2026 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef BLUETOOTH_DEVICE_BATTERY_OBSERVER_STUB_H
+#define BLUETOOTH_DEVICE_BATTERY_OBSERVER_STUB_H
+
+#include <map>
+#include "iremote_stub.h"
+#include "i_bluetooth_device_battery_observer.h"
+#include "bluetooth_service_ipc_interface_code.h"
+
+namespace OHOS {
+namespace Bluetooth {
+class BluetoothDeviceBatteryObserverStub : public IRemoteStub<IBluetoothDeviceBatteryObserver> {
+public:
+    BluetoothDeviceBatteryObserverStub();
+    ~BluetoothDeviceBatteryObserverStub();
+
+    int32_t OnRemoteRequest(uint32_t code, MessageParcel &data, MessageParcel &reply, MessageParcel &option) override;
+
+private:
+    ErrCode OnGetBatteryLevelEventInner(MessageParcel &data, MessageParcel &reply);
+    ErrCode OnBatteryLevelChangedInner(MessageParcel &data, MessageParcel &reply);
+
+    std::map<uint32_t,
+        ErrCode (BluetoothDeviceBatteryObserverStub::*)(MessageParcel &data, MessageParcel &reply)>
+        memberFuncMap_;
+    DISALLOW_COPY_ADN_MOVE(BluetoothDeviceBatteryObserverStub);
+};
+} // namespace Bluetooth
+} // namespace OHOS
+#endif
